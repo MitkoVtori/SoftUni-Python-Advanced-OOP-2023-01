9a1109449d04751b651d1b241c008aac0278bd3a
Mikto.Vtori <gamingplayerdm@gmail.com>
2022-12-21 13:27:52 +0200

    added Functions Advanced - Lab

---
+++ Advanced/3.Functions Advanced/Functions Advanced - Lab/01. Multiplication Function.py
@@ -0,0 +1,28 @@
+def multiply(*args):
+    '''
+        This is a simple multiplication function that
+        gets multiple elements and returns their multiplication sum
+    '''
+
+    result = 1 # result is set to one, because we do multiplication, and anything multiplied by 0 equals 0.
+
+    for item in args:
+
+        if type(item) is int:
+
+            result *= item
+
+    return result
+
+
+''' No string tests '''
+
+# print(multiply(1, 4, 5))
+# print(multiply(4, 5, 6, 1, 3))
+# print(multiply(2, 0, 1000, 5000))
+
+''' String tests '''
+
+# print(multiply(1, "Henry", 45))
+# print(multiply("First", "class"))
+# print(multiply())
---
+++ Advanced/3.Functions Advanced/Functions Advanced - Lab/06. Recursive Power.py
@@ -0,0 +1,21 @@
+def recursive_power(number, power):
+    if power == 0:
+        return 1
+    return number * recursive_power(number, power - 1)
+
+
+''' --- Tests --- '''
+# print(recursive_power(2, 10))
+# print(recursive_power(10, 100))
+
+
+
+
+# ''' Without recurse '''
+# def recursive_power(number, power):
+#     return number ** power
+
+
+# ''' --- Tests --- '''
+# print(recursive_power(2, 10))
+# print(recursive_power(10, 100))
