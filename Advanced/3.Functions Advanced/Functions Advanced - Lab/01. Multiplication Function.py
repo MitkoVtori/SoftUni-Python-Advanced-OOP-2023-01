029cff1242dc9d352a3e983f8b78c290e308e729
Mikto.Vtori <gamingplayerdm@gmail.com>
2023-01-25 15:26:47 +0200

    Redid Functions Advanced - Lab

--- Advanced/3.Functions Advanced/Functions Advanced - Lab/01. Multiplication Function.py
+++ Advanced/3.Functions Advanced/Functions Advanced - Lab/01. Multiplication Function.py
@@ -26,3 +26,17 @@ def multiply(*args):
 # print(multiply(1, "Henry", 45))
 # print(multiply("First", "class"))
 # print(multiply())
+
+
+
+# def multiply(*args):
+#     result = args[0]
+#     for num in args[1:]:
+#         result *= num
+#     return result
+#
+#
+# ''' TESTS '''
+# # print(multiply(1, 4, 5))
+# # print(multiply(4, 5, 6, 1, 3))
+# # print(multiply(2, 0, 1000, 5000))
--- Advanced/3.Functions Advanced/Functions Advanced - Lab/06. Recursive Power.py
+++ Advanced/3.Functions Advanced/Functions Advanced - Lab/06. Recursive Power.py
@@ -1,6 +1,6 @@
 def recursive_power(number, power):
-    if power == 0:
-        return 1
+    if power == 1:
+        return number
     return number * recursive_power(number, power - 1)
 
 
@@ -8,6 +8,13 @@ def recursive_power(number, power):
 # print(recursive_power(2, 10))
 # print(recursive_power(10, 100))
 
+
+
+''' ANOTHER WAY '''
+# def recursive_power(number, power):
+#     if power == 0:
+#         return 1
+#     return number * recursive_power(number, power - 1)
 
 
 
